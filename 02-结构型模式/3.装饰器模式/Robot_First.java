4650805c59c151f3e151b78a0f87cefe381c2bf7
McQ <2810806242@qq.com>
2023-10-04 11:14:49 +0800

    结构型模式完毕

--- 02-结构型模式/3.装饰器模式/Robot_First.java
+++ 02-结构型模式/3.装饰器模式/Robot_First.java
@@ -2,7 +2,7 @@
 public class Robot_First implements Robot{
     @Override
     public void doSomeThing() {
-        System.out.println("唱歌 ");
+        System.out.println("唱歌");
         System.out.println("对话");
     }
 }
